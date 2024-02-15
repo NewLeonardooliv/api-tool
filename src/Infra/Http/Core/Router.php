a4c3ce44cc624159cfb525e89e137c5af7dc8464
Leonardo Oliveira <leonardo_oliveira7@hotmail.com>
2024-02-15 00:34:37 -0300

    refact: Router class

--- src/Infra/Http/Core/Router.php
+++ src/Infra/Http/Core/Router.php
@@ -7,12 +7,9 @@ use App\Infra\Http\Core\Errors\InvalidRoute;
 
 class Router
 {
-    private const GET = 'GET';
-    private const POST = 'POST';
-    private const PUT = 'PUT';
-    private const PATH = 'PATH';
+    private const METHODS = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'];
 
-    public static array $routes;
+    private static array $routes = [];
 
     public static function findRoute(string $method, string $path)
     {
@@ -23,30 +20,46 @@ class Router
         return self::$routes[$method][$path];
     }
 
-    public static function post(string $path, Controller $controller)
+    public static function addRoute(string $method, string $path, Controller $controller)
     {
-        self::$routes[self::POST][$path] = $controller;
+        self::validate($method, $path);
+
+        self::$routes[$method][$path] = $controller;
     }
 
     public static function get(string $path, Controller $controller)
     {
-        self::$routes[self::GET][$path] = $controller;
+        self::addRoute('GET', $path, $controller);
+    }
+
+    public static function post(string $path, Controller $controller)
+    {
+        self::addRoute('POST', $path, $controller);
     }
 
     public static function put(string $path, Controller $controller)
     {
-        self::$routes[self::PUT][$path] = $controller;
+        self::addRoute('PUT', $path, $controller);
     }
 
-    public static function path(string $path, Controller $controller)
+    public static function patch(string $path, Controller $controller)
     {
-        self::$routes[self::PATH][$path] = $controller;
+        self::addRoute('PATCH', $path, $controller);
+    }
+
+    public static function delete(string $path, Controller $controller)
+    {
+        self::addRoute('DELETE', $path, $controller);
     }
 
     private static function validate(string $method, string $path)
     {
+        if (!in_array($method, self::METHODS)) {
+            throw new InvalidRoute('Invalid HTTP method specified');
+        }
+
         if (self::pathExists($method, $path)) {
-            throw new InvalidRoute('Method already exists.');
+            throw new InvalidRoute('Route already exists for the method');
         }
     }
 
--- src/Infra/Http/Routes/Routes.php
+++ src/Infra/Http/Routes/Routes.php
@@ -1,3 +1,15 @@
 <?php
 
+use App\Infra\Contracts\Controller;
+use App\Infra\Http\Core\Router;
 
+class MeuController implements Controller
+{
+    public function handle()
+    {
+        print 'aqui';
+    }
+}
+
+Router::get('/leonardo', new MeuController());
+Router::post('leonardo', new MeuController());
