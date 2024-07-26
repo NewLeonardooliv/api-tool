a4149592e87fe59e813577d35f88b98bb15a6973
Leonardo Oliveira <leonardo@ipeweb.com.br>
2024-07-26 19:54:26 -0300

    feat. patterns

--- src/Modules/MarkDown/UseCases/MarkDownToJson/MarkDownToJsonUseCase.php
+++ src/Modules/MarkDown/UseCases/MarkDownToJson/MarkDownToJsonUseCase.php
@@ -6,6 +6,26 @@ use App\Infra\Contracts\UseCase;
 
 class MarkdownToJsonConverter implements UseCase
 {
+    public const PATTERN_HEADER_LEVEL_ONE = '/^# (.*)$/';
+    public const PATTERN_HEADER_LEVEL_TWO = '/^## (.*)$/';
+    public const PATTERN_LIST_ITEM = '/^\* (.*)$/';
+    public const PATTERN_LINK = '/^\[(.*)\]\((.*)\)$/';
+    public const PATTERN_IMAGE = '/^!\[(.*)\]\((.*)\)$/';
+    public const PATTERN_BLOCK_QUOTE = '/^> (.*)$/';
+    public const PATTERN_INLINE_CODE = '/`([^`]*)`/';
+    public const PATTERN_CODE_BLOCK = '/^```/';
+
+    public const PATTERNS = [
+        self::PATTERN_HEADER_LEVEL_ONE => ['type' => 'header', 'level' => 1, 'text' => 1],
+        self::PATTERN_HEADER_LEVEL_TWO => ['type' => 'header', 'level' => 2, 'text' => 1],
+        self::PATTERN_LIST_ITEM => ['type' => 'list_item', 'text' => 1],
+        self::PATTERN_LINK => ['type' => 'link', 'text' => 1, 'href' => 2],
+        self::PATTERN_IMAGE => ['type' => 'image', 'alt' => 1, 'src' => 2],
+        self::PATTERN_BLOCK_QUOTE => ['type' => 'block_quote', 'text' => 1],
+        self::PATTERN_INLINE_CODE => ['type' => 'inline_code', 'code' => 1],
+        self::PATTERN_CODE_BLOCK => 'code_block',
+    ];
+
     public function execute()
     {
         $markdownText = <<<'MD'
@@ -44,18 +64,7 @@ class MarkdownToJsonConverter implements UseCase
 
     private function parseLine(string $line): array
     {
-        $patterns = [
-            '/^# (.*)$/' => ['type' => 'header', 'level' => 1, 'text' => 1],
-            '/^## (.*)$/' => ['type' => 'header', 'level' => 2, 'text' => 1],
-            '/^\* (.*)$/' => ['type' => 'list_item', 'text' => 1],
-            '/^\[(.*)\]\((.*)\)$/' => ['type' => 'link', 'text' => 1, 'href' => 2],
-            '/^!\[(.*)\]\((.*)\)$/' => ['type' => 'image', 'alt' => 1, 'src' => 2],
-            '/^> (.*)$/' => ['type' => 'block_quote', 'text' => 1],
-            '/`([^`]*)`/' => ['type' => 'inline_code', 'code' => 1],
-            '/^```/' => 'code_block',
-        ];
-
-        foreach ($patterns as $pattern => $structure) {
+        foreach (self::PATTERNS as $pattern => $structure) {
             if (preg_match($pattern, $line, $matches)) {
                 if ($structure === 'code_block') {
                     return $this->parseCodeBlock($line);
